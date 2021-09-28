fd26ee4caf1789df617fb2cc7e369734056e6f66
JW314 <jack.t.y.wu@gmail.com>
2021-09-28 08:17:23 -0400

    Board Adj. List
    Added an adjacency list for the square board which will be useful later for routing purposes I think
    
    Later I might add a specific diagonal list as well for diagonal movements

--- World/Board.gd
+++ World/Board.gd
@@ -5,6 +5,8 @@ var separation_value = -75
 var board_length = 10
 var board_width = 10
 
+var adjacency_list = []
+
 #Dictionary containing different states of a tile
 const board_data = {
 	onFire = false,
@@ -24,6 +26,18 @@ func create_board_matrix():
 			board_matrix[x].append(board_data.duplicate())
 			#Assigning global position for card to take in
 			board_matrix[x][y].card_position = Vector2(x * separation_value + 350, y * separation_value + 350)
+
+	for x in range(board_width):
+		for y in range(board_length):
+			var thiscellID = 10*x+y
+			var adjacent_cells = [thiscellID + 1, thiscellID - 1, thiscellID + board_width, thiscellID - board_width]
+			var adjacent_filtered = []
+			for adj in adjacent_cells:
+				if(adj >= 0 && adj < board_width * board_length):
+					adjacent_filtered.append(adj)
+			adjacency_list.append([adjacent_filtered])
+			
+		
 
 func create_pentagon_matrix():
 	
--- World/World.gd
+++ World/World.gd
@@ -10,5 +10,5 @@ func _ready():
 
 func put_card(card_pos):
 	var basic_card = Basic_card.instance()
-	self.add_child(basic_card)
+	#self.add_child(basic_card)
 	basic_card.global_position = card_pos
