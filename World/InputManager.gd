7e29ca886a3af05823b2f4824d526673c6680e1f
sidd <n2verz@gmail.com>
2021-10-04 10:23:16 -0400

    add basic query

--- World/InputManager.gd
+++ World/InputManager.gd
@@ -1,4 +1,38 @@
 extends Node
 
-func move(sl):
-	print('hello')
+enum {
+	MOVE,
+	ATTACK,
+	ABILITY
+}
+
+var state = MOVE
+var tileArray = []
+var requiredTiles = null
+var tile = null
+var tileTwo = null
+
+func decide_input(tile_id):
+	take_input(tile_id, state)
+	match state:
+		MOVE:
+			requiredTiles = 2
+			move(tile_id)
+
+#Queries
+func take_input(tile_id, state):
+	if tileArray != null:
+		if tileArray.size() >= requiredTiles:
+			return
+	tileArray.push_back(tile_id)
+
+func move(event_position):
+	check_tiles()
+	#TODO - Does Board Action/Calls Function
+
+#Checks required Tiles and if its good, it clears query
+func check_tiles():
+	if tileArray.size() != requiredTiles:
+		return
+	tileArray.clear()
+	requiredTiles = null
