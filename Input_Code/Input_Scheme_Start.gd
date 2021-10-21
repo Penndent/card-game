1750f497822604169c2b9b8d0def30d561ed058e
JW314 <jack.t.y.wu@gmail.com>
2021-10-21 09:49:46 -0400

    input cleanup

--- Input_Code/Input_Scheme_Start.gd
+++ Input_Code/Input_Scheme_Start.gd
@@ -5,16 +5,13 @@ class_name Input_Scheme_Start
 const IS_Unit = preload("res://Input_Code/Input_Scheme_Unit.gd")
 
 
-func _init(hist):
-	input_piece_history = hist
+func _init():
 	status_message = "Start"
 	pass
 
-func process_piece(input_piece):
-	var hist_copy = input_piece_history.duplicate()
-	hist_copy.append(input_piece)
-	var next_input_scheme = IS_Unit.new(hist_copy)
+func process_piece(new_piece, history):
+	var next_input_scheme = IS_Unit.new()
 	var exes = []
-	exes.append(Executor_Status.new(input_piece_history, self, next_input_scheme))
-	return Scheme_Output.new(null, exes);
+	exes.append(Executor_Status.new(history, self, next_input_scheme))
+	return Scheme_Output.new(next_input_scheme, exes);
 
